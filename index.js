ff33ac6a47059af554fa15b84471f0a566c3d158
Julia Johnson <julialj95@gmail.com>
2021-07-06 21:47:18 -0500

    displaying cards

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 let deckId;
+let cardButton = document.getElementById("draw-cards");
+let cardDiv = document.getElementById("cards");
 
-function getDeck() {}
+function getDeck() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      deckId = data.deck_id;
+      cardButton.classList.toggle("hidden");
+    })
+    .catch((error) => console.log(error));
+}
+
+function drawCards() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards);
+      cardDiv.innerHTML = `<img src=${data.cards[0].image} /><br><img src=${data.cards[1].image} />`;
+    })
+    .catch((error) => console.log(error));
+}
+
+document.getElementById("new-deck").addEventListener("click", getDeck);
+cardButton.addEventListener("click", drawCards);
